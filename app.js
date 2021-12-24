c8105fcf735ea93044eda474241412bbe15a4eac
Dewanand khilar <dewanandkhilar@gmail.com>
2021-12-24 20:25:20 +0530

    changes

--- app.js
+++ app.js
@@ -36,7 +36,11 @@ app.get("/compose", (req, res)=> {
 });
 
 app.post("/compose", (req, res)=> {
- console.log(req.body.postTitle);
+ const post = {
+   title: req.body.postTitle,
+   content: req.body.postBody
+ };
+
 });
 
 
